12b4f86606b2192fcc59a32f8c498a9af958068d
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-09-13 23:37:48 +0200

    added support for chainId and networkId as numbers

--- src/lookup.js
+++ src/lookup.js
@@ -4,9 +4,17 @@ function lookup(
     networkId,
     chainId = null
 ) {
+    if (typeof networkId === 'number') {
+        networkId = '0x' + networkId.toString(16);
+    }
+
     const networks = atlas.filter((network) => network.networkId.toLowerCase() === networkId.toLowerCase());
 
     if (chainId) {
+        if (typeof chainId === 'number') {
+            chainId = '0x' + chainId.toString(16);
+        }
+
         const network = networks.find((network) => network.chainId &&
             network.chainId.toLowerCase() === chainId.toLowerCase());
 
